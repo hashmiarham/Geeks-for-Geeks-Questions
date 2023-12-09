e32fd135a5d22d04fe4344fc6fa16f67e43a27c1
Arham Zafar Hashmi <75936363+hashmiarham@users.noreply.github.com>
2023-12-09 23:35:13 +0530

    Add files via upload

---
+++ Print 1 To N Without Loop.py
@@ -0,0 +1,8 @@
+#User function Template for python3
+
+class Solution:    
+    #Complete this function
+    def printNos(self,N):
+        #Your code here
+        for n in range(1,N+1):
+            print(n,end=" ")
